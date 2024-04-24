acfcfbf43ac5640e113e025802401aac3e88dd07
rosiearasa <rosearasa7522@gmail.com>
2024-04-25 02:55:11 +0300

    flutter reactive and flutter fire dependencies added

---
+++ bobber/lib/services/flutter_reactive_ble.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+
+final flutterReactiveBle = FlutterReactiveBle();
+
+//scan for devices, O: Results/Error 
+
+
+//Establish a connection
+
+
+//read&write data
